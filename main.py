8a016e5dd92287acb75745b99efbd4d4aedd6406
mfantina <m.fantinato@usp.br>
2019-12-01 00:19:15 -0300

    regular

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ from datetime import datetime
 from functools import partial
 
 numberOfParametersCombinations = 1
-numberOfThreads = 444
-numberOfRounds = 555
+numberOfThreads = 5
+numberOfRounds = 1
 
 def runRound(par, parComb, parCount, numberOfThreads, round, broadcast, messenger, islandSizes, percentageOfBestIndividualsForMigrationAllIslands, taskAddition, sampledLog, fullLog, alphabet, islandNumber):              # added "messenger" + "taskAddition"  [hiddenTasks]
     islandStart = datetime.now()
