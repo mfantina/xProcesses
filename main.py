ea59d83cee0616ea25e74de4339d28d5c92672ab
mfantina <m.fantinato@usp.br>
2019-12-01 00:04:08 -0300

    test

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from datetime import datetime
 from functools import partial
 
 numberOfParametersCombinations = 1
-numberOfThreads = 567
+numberOfThreads = 1
 numberOfRounds = 1
 
 def runRound(par, parComb, parCount, numberOfThreads, round, broadcast, messenger, islandSizes, percentageOfBestIndividualsForMigrationAllIslands, taskAddition, sampledLog, fullLog, alphabet, islandNumber):              # added "messenger" + "taskAddition"  [hiddenTasks]
